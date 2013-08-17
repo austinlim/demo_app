1c6abff20962a9c90e7c877ad10f68488e436116
cclim <austincclim@gmail.com>
2013-08-17 15:56:05 +0800

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
